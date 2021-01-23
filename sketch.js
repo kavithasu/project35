3151b35f40d9839aa5a844be63b7ead5f735cba7
kavithasu <72619347+kavithasu@users.noreply.github.com>
2021-01-23 13:04:04 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,30 @@
+var dog,happydog;
+var database;
+var foodS,foodstock;
+
+
+function preload()
+{
+  dog=loadImage("dogImage.png");
+  happydog=loadImage("dogImg1.png");
+
+}
+
+function setup() {
+  createCanvas(500,500);
+  
+  dog=createSprite(250,250,50,50);
+  dog.addimage
+  
+}
+
+
+function draw() {  
+
+  drawSprites();
+  //add styles here
+
+}
+
+
+
